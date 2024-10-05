51292c930a156152f02f6b493b51fb253e54382a
Bilal Layache <bilal.layache@gmail.com>
2024-10-05 08:41:49 +0200

    Repository User and Activity created

---
+++ users/src/main/java/com/users/users/model/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.users.users.model.repository;
+
+import com.users.users.model.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String email);
+}
